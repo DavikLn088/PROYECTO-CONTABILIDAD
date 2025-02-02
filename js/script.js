b82416383d56bdaa1f987651803754063daf347c
Jeremy Burgos <jeremy_santi1@outlook.com>
2025-02-02 01:05:32 -0500

    SE PUSO EL CODIGO DE FUNCIONES DE LOGIN Y REGISTER

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+//Declaración de variables
+var formulario_login = document.querySelector(".formulario__login");
+var formulario_register = document.querySelector(".formulario__register");
+function register{
+
+
+
+}
+
+
