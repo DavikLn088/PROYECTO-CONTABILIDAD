1870e447c045758e76ed5cf1cf79a562efe4c125
Jeremy Burgos <jeremy_santi1@outlook.com>
2025-02-03 18:37:06 -0500

    CORRECION PIXELES JS.SCRIPT

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ function iniciarSesion(){
 function register(){
     if(window.innerWidth > 850){
         formulario_register.style.display = "block";
-        contenedor_login_register.style.left = "400px";
+        contenedor_login_register.style.left = "410px";
         formulario_login.style.display = "none";
         caja_trasera_register.style.opacity = "0";
         caja_trasera_login.style.opacity = "1";
