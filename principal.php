e0a7b8ce1ce77766e48ca457645e7e4cc5a03b9d
John Doe <johndoe@example.com>
2025-02-18 12:57:44 -0500

    INGRESO DE ROL DE PAGOS BASICO SIN DISEÑO

--- principal.php
+++ principal.php
@@ -15,13 +15,23 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Principal</title>
+    <link rel="stylesheet" href="./css/style_principal.css">
 </head>
 <body>
-    <h1>Bienvenido a la página principal</h1>
-    <a href="./php/cerrar_sesion.php">Cerrar sesion</a>
+    
+    <main>
+        <div class="contenedor__todo">
+            <div class="caja_rol_de_pagos">
+                <div class="caja__trasera_rol">
+                    <h3>Rol de pagos</h3>
+                            <button id="btn__ingresar">Ingresar valores</button>
+                </div>
+        <div class="cerrar_sesion">
+        <button id="btn_cerrar_sesion"><a href="./php/cerrar_sesion.php">Cerrar sesion</a></button>
 </body>
 </html>
