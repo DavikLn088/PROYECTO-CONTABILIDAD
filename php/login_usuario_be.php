1a0e639c3888b370a11cbf77ddf1916f781bdf2b
Jeremy <hola@gmail.com>
2025-02-18 10:35:17 -0500

    AREGLAR PROBLEMAS DE SESION

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -1,38 +1,24 @@
 <?php
-
+    session_start();
     include 'conexion_be.php';
 
     $correo = $_POST['correo'];
-    $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
+    $contrasena = hash('sha512', $contrasena);
 
-    $validar_login1 = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and
+    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and
     contrasena='$contrasena'");
-    if(mysqli_num_rows($validar_login1) > 0){
-        header("location:../principal.php");
+    if(mysqli_num_rows($validar_login) > 0){
+        $_SESSION['usuario'] = $correo;
+        header("location: ../principal.php");
         exit;
     }else{
         echo '
             <script>
-                alert("Correo o Usuario no encontrados, por favor verifique los datos introducidos");
+                alert("Usuario no encontrado, por favor verifique los datos introducidos");
                 window.location = "../index.php";
             </script>
         ';
         exit;
     }
-    $validar_login2 = mysqli_query($conexion, "SELECT * FROM usuarios WHERE contrasena='$contrasena'");
-    if(mysqli_num_rows($validar_login2) > 0){
-        header("location:../principal.php");
-        exit;
-    }else{
-        echo '
-            <script>
-                alert("Contraseña incorrecta, por favor verifique los datos introducidos");
-                window.location = "../index.php";
-            </script>
-        ';
-        exit;
-    }
-
-
 ?>
--- principal.php
+++ principal.php
@@ -1,3 +1,18 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['usuario'])){
+        echo '
+            <script>
+                alert("Por favor debes iniciar sesion");
+                window.location = "index.php";
+            </script>
+        ';
+        session_destroy();
+        die();
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,5 +22,6 @@
 </head>
 <body>
     <h1>Bienvenido a la página principal</h1>
+    <a href=".php/cerrar_sesion.php">Cerrar sesion</a>
 </body>
 </html>
